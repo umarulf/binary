2d95749f8279aa4d2fb8648ec6c1e92bf5f9928c
umarul-fr <umarul.f@edstem.com>
2023-12-26 16:34:01 +0530

    made changes
    made changes

--- my-app/src/BinarySearch.js
+++ my-app/src/BinarySearch.js
@@ -1,82 +1,122 @@
 import React, { useState } from "react";
+import "./BinarySearch.css";
+
+const Block = ({ height, index, value }) => {
+  return (
+    <div
+      className="block"
+      style={{ height: `${height}px`, transform: `translate(${index * 30}px)` }}
+    >
+      <label className="block_id">{value}</label>
+    </div>
+  );
+};
 
 const BinarySearch = () => {
-  const [arrayInput, setArrayInput] = useState("");
-  const [target, setTarget] = useState("");
-  const [result, setResult] = useState(null);
+  const [array, setArray] = useState([]);
+  const [inputArray, setInputArray] = useState("");
+  const [searchResult, setSearchResult] = useState("");
 
-  const binarySearch = (arr, target) => {
-    let left = 0;
-    let right = arr.length - 1;
+  const handleInputChange = (e) => {
+    setInputArray(e.target.value);
+  };
 
-    while (left <= right) {
-      const mid = Math.floor((left + right) / 2);
+  const setUserArray = () => {
+    const userInputArray = inputArray
+      .split(",")
+      .map((num) => parseInt(num.trim()));
+    setArray(userInputArray);
+  };
 
-      if (arr[mid] === target) {
-        return mid;
-      } else if (arr[mid] < target) {
-        left = mid + 1;
+  const highlightBlock = (index, color) => {
+    const blocks = document.querySelectorAll(".block");
+    blocks[index].style.backgroundColor = color;
+  };
+
+  const binarySearch = async (num) => {
+    const blocks = document.querySelectorAll(".block");
+
+    for (let i = 0; i < blocks.length; i += 1) {
+      highlightBlock(i, "blue");
+    }
+
+    let start = 0;
+    let end = array.length - 1;
+    let flag = 0;
+
+    while (start <= end) {
+      const mid = Math.floor((start + end) / 2);
+      highlightBlock(mid, "red");
+      const value = array[mid];
+
+      await new Promise((resolve) => setTimeout(resolve, 900));
+
+      if (value === num) {
+        setSearchResult(<span className="found">Element Found</span>);
+        highlightBlock(mid, "green");
+        flag = 1;
+        break;
+      }
+
+      if (value > num) {
+        end = mid - 1;
+        highlightBlock(mid, "blue");
       } else {
-        right = mid - 1;
+        start = mid + 1;
+        highlightBlock(mid, "blue");
       }
     }
 
-    return -1;
-  };
-
-  const handleSearch = () => {
-    const array = arrayInput.split(",").map((num) => parseInt(num, 10));
-    const index = binarySearch(array, parseInt(target, 10));
-    setResult(index);
+    if (flag === 0) {
+      setSearchResult(<span className="not-found">Element Not Found</span>);
+    }
   };
 
   return (
-    <div style={{ textAlign: "center" }}>
-      <h2 style={{ fontSize: "24px" }}>Binary Search</h2>
-      <div>
-        <label style={{ fontWeight: "bold", fontSize: "18px" }}>
-          Array:
-          <input
-            type="text"
-            value={arrayInput}
-            onChange={(e) => setArrayInput(e.target.value)}
-            style={{ fontSize: "16px" }}
-          />
-        </label>
+    <div>
+      <br />
+      <p className="header">Binary Search</p>
+
+      <div id="array">
+        {array.map((value, index) => (
+          <Block key={index} value={value} index={index} height={value * 3} />
+        ))}
       </div>
-      <div>
-        <label style={{ fontWeight: "bold", fontSize: "18px" }}>Target:</label>
+      <br />
+      <br />
+
+      <div style={{ textAlign: "center" }}>
+        <label htmlFor="arrayInput">Enter comma-separated numbers:</label>
         <input
-          type="number"
-          value={target}
-          onChange={(e) => setTarget(e.target.value)}
-          style={{ fontSize: "16px" }}
+          type="text"
+          id="arrayInput"
+          value={inputArray}
+          onChange={handleInputChange}
         />
+        <br />
+        <br />
+        <button id="btn" onClick={setUserArray}>
+          Set Array
+        </button>
+        <br />
+        <br />
+        <label htmlFor="searchNumber">Number to be Searched:</label>
+        <input type="text" id="searchNumber" name="searchNumber" />
+        <br />
+        <br />
         <button
-          onClick={handleSearch}
-          style={{
-            marginLeft: "5px",
-            padding: "5px",
-            backgroundColor: "green",
-            color: "white",
-            cursor: "pointer",
-            border: "none",
-            borderRadius: "3px",
-            fontSize: "16px",
-          }}
+          id="searchBtn"
+          onClick={() =>
+            binarySearch(
+              parseInt(document.getElementById("searchNumber").value, 10)
+            )
+          }
         >
           Search
         </button>
-      </div>
-      <div>
-        <label style={{ fontWeight: "bold", fontSize: "18px" }}>
-          Result:{" "}
-          {result !== null
-            ? result !== -1
-              ? `Found at index ${result}`
-              : "Not found"
-            : ""}
-        </label>
+        <br />
+        <br />
+        <div id="text">{searchResult}</div>
       </div>
     </div>
   );
